31eeb8692486e5c7e9f586fe657571071603cdc9
mandeepsidhu <mandeepsidhu67588@yahoo.com>
2023-06-14 21:43:32 -0400

    Add files via upload

--- database/migrations/20220113221856_intial_db_migration.js
+++ database/migrations/20220113221856_intial_db_migration.js
@@ -1,6 +1,7 @@
 exports.up = async function (knex) {
   await knex.schema.createTable("materials", function (t) {
     t.increments("id").unsigned().primary();
+    t.string("name").notNullable();
     t.integer("power_level");
     t.integer("qty");
     t.timestamp("deleted_at");
